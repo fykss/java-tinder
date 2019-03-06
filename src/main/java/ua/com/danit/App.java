796d9f7c030fe56caf1317e89ba3d48391157473
Dima Onopko <onopko24@gmail.com>
2019-03-06 17:36:58 +0200

    FINAL

--- src/main/java/ua/com/danit/App.java
+++ src/main/java/ua/com/danit/App.java
@@ -25,7 +25,7 @@ public class App {
 
         String port = System.getenv().get("PORT");
         if (port == null || port == ""){
-            port="8080";
+            port="8082";
         }
 
         Server server = new Server(Integer.parseInt(port));
